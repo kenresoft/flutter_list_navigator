ecc67f22e4e510e5887ac0878f7e088ad88e1929
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-05-13 10:45:57 +0100

    Update to latest version of flutter.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\user\AndroidStudioPlugins\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\user\AndroidStudioProjects\PROJECTS\flutter_list_navigator"
+export "FLUTTER_APPLICATION_PATH=C:\Users\user\AndroidStudioProjects\PROJECTS\packages\flutter_list_navigator"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
